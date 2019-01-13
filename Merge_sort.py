a23873c2bb595bd212f7395047a428f03b6758e4
Jamil Mbabaali <jamilnineteen91@gmail.com>
2019-01-13 14:08:33 -0600

    Update Merge_sort.py

--- Merge_sort.py
+++ Merge_sort.py
@@ -16,29 +16,30 @@ def Merge_sort(nums):
 
 def merge(left, right):
     sorted_list=[]
-    index_L=0 #index_L is used to incrementally ascend the group a list
-    index_R=0 #index_R is used to incrementally ascend the group b lis
+    index_L=0 #index_L is used to incrementally ascend the left list.
+    index_R=0 #index_R is used to incrementally ascend the right list.
 
     #Lists containing more than one item will enter the while loop where they'll be sorted.
     while index_L < len(left) and index_R < len(right):
-            #Prints left & right groups that are entering the while loop
+            #Prints left & right groups that are have entered the while loop.
             print(left[index_L:], right[index_R:])
 
             if left[index_L]<=right[index_R]:
                 sorted_list.append(left[index_L])
                 index_L+=1
 
-                #Prints the list of item/s (the smallest item between the left group and right group) to the new_list).
+ #Prints the current sorted_list state, the smallest item between the left group and right group has been inserted into sorted_list.
                 print(sorted_list)
             else:
                 sorted_list.append(right[index_R])
                 index_R+=1
 
-                #Prints the list of item/s (the smallest item between the left group and right group) to the new_list).
+ #Prints the current sorted_list state, the smallest item between the left group and right group has been inserted into sorted_list.
                 print(sorted_list)
 
     #Lists containing one item will be added to the sorted_list.
     #The append function is unable to append lists into new_list, hence why'+=' is used.
+    #Unable to use 'index_L' as a list index since the incrementation only takes place in the while loop,hence why 'index_L:' and 'index_R:' are used.
     sorted_list+= left[index_L:]
     sorted_list+= right[index_R:]
     return sorted_list
