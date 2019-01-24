3a2a2507b90fd662648f12b0f00757410937528c
Jamil Mbabaali <jamilnineteen91@gmail.com>
2019-01-23 22:55:57 -0600

    eliminated recursivealgorithm, was inefficient

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,6 +1,6 @@
 nums = [12,4,56,4,13,764,3,76]
 
-# O(n^2)
+
 def Insertion_sort(nums):
     for i in range(1,len(nums)):
         for j in range(i-1,-1,-1):
@@ -13,15 +13,4 @@ def Insertion_sort(nums):
 
 print(Insertion_sort(nums))
 
-# Recursive Insertion Sort
-#O(n)
-def Rec_Insertion_Sort(nums):
-    for i in range(1,len(nums)):
-        if nums[i-1]>nums[i]:
-            nums[i-1],nums[i]=nums[i],nums[i-1]
-            Rec_Insertion_Sort(nums)
-        else:
-            pass
-    return nums
 
-print(Rec_Insertion_Sort(nums))
